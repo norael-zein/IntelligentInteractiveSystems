ca60a90cc1a837e2860d49f8db585e8851b9a517
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-12-03 23:28:02 +0100

    added models

--- scripts/subSystem1/model1.py
+++ scripts/subSystem1/model1.py
@@ -14,7 +14,8 @@ df = pd.read_csv("dataset/test/dataset.csv")
 emotion = df["emotion"]
 inputs = df.drop(labels="emotion", axis=1)
 
-#Do a balanced split of the dataset for train/val/test: I decided to split into 70/20/10:
+#Do a balanced split of the dataset for train/val/test: We decided to split into 70/20/10:
+
 #Training and Validation/Test: 90/10
 X_train_val, X_test, y_train_val, y_test = train_test_split(
     inputs,
@@ -32,102 +33,47 @@ X_train, X_val, y_train, y_val = train_test_split(
     random_state=42)
 print("\nSplit of the data: ", len(X_train), len(X_val),len(X_test), "\n")
 
-#KNeighborsClassifier() as model
-def KN_classifier():
-    #Hyperparameter search/tuning using the training and validation set
-    param_grid = {
-    'n_neighbors': [i for i in range(20)],
-    'weights': ['uniform', 'distance'],
-    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
-    }
+models_and_params = [
+    ("KNN", KNeighborsClassifier(), {
+        'n_neighbors': [15,19,20,21,22,23,24],
+        'weights': ['uniform', 'distance'],
+        'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+    }),
+    ("SVC", SVC(), {
+        "kernel": ["poly", "rbf", "linear", "sigmoid"],
+        "degree": [1,5,10,15]
+    }),
+    ("Random Forest", RandomForestClassifier(), {
+        "n_estimators": [100,150,200,500],
+        "max_features": ['sqrt', 'log2'],
+        "max_depth": [4, 5, 6, 7, 8],
+        "criterion": ['gini', 'entropy']
+    }),
+    ("Decision Tree", DecisionTreeClassifier(), {
+        'criterion': ['gini', 'entropy'],
+        'max_depth': [1,2,3,4,5,6,7,8,10,20,50,100]
+    }),
+    ("Logistic Regression", LogisticRegression(), {
+        'penalty': ['l1', 'l2'],
+        'C': [1,2,3,4,5,6,7,8,10,100,1000],
+        'solver': ['liblinear']
+    }),
+    ("Gradient Boosting", GradientBoostingClassifier(), {
+        "max_depth": [3,5,7,8,9,10,11,20,25],
+        "max_features": ["log2", "sqrt"],
+        "subsample": [0.5, 0.618, 0.8, 0.85, 0.9, 0.95, 1.0]
+    })
+]
 
-    best_model = GridSearchCV(KNeighborsClassifier(), param_grid)
-    best_model.fit(X_train_val, y_train_val)
+#When using GridSearchCV, we split the training/validation data internally and can use all the data from the training/validation datasets
+for model_name, model, param_grid in models_and_params:
+    
+    current_model = GridSearchCV(model, param_grid)
+    current_model.fit(X_train_val, y_train_val) 
 
-    #Analyze performance on selected model with selected hyperparameters on test set
-    print("\n\nPrediction on kNN with best parameters on test set:",accuracy_score(y_test, best_model.predict(X_test)))
-    print("\nBest parameters: ",best_model.best_params_)
-
-    return best_model
-
-#SVC classification
-def SVC_classification():
-    param_grid = [
-        {"kernel": ["poly"], "degree": [i for i in range(50)]},
-        {"kernel": ["rbf", "linear", "sigmoid"]}
-    ]
-    best_model = GridSearchCV(SVC(), param_grid)
-    best_model.fit(X_train_val, y_train_val) 
-
-    print("\n\nBest model with best parameters on test set: ",
-          accuracy_score(y_test, best_model.predict(X_test)))
-    print("Best parameters of best model: ",best_model.best_params_)
-
-    return best_model
-
-#Random forest
-def random_forest():
-    param_grid = [
-        {"n_estimators": [200, 500]},
-        {"max_features": ['auto', 'sqrt', 'log2']},
-        {"max_depth": [4,5,6,7,8]},
-        {"criterion":['gini', 'entropy']}
-    ]
-
-    best_model = GridSearchCV(RandomForestClassifier(), param_grid)
-    best_model.fit(X_train_val, y_train_val) 
-
-    print("\n\nBest model with best parameters on test set: ",
-          accuracy_score(y_test, best_model.predict(X_test)))
-    print("Best parameters of best model: ",best_model.best_params_)
-
-    return best_model
-
-#Decision tree
-def decision_tree():
-    param_grid = [
-        {'criterion':['gini','entropy']},
-        {'max_depth':[4,5,6,7,8,9,10,11,12,15,20,30,40,50,70,90,120,150]}
-    ]
-
-    best_model = GridSearchCV(DecisionTreeClassifier(), param_grid)
-    best_model.fit(X_train_val, y_train_val) 
-
-    print("\n\nBest model with best parameters on test set: ",
-          accuracy_score(y_test, best_model.predict(X_test)))
-    print("Best parameters of best model: ",best_model.best_params_)
-
-    return best_model
-
-#Logistic regression
-def logistic_regression():
-    param_grid = [
-            {'penalty':['l1','l2']}, 
-            {'C':[1, 10, 100, 1000]}
-            ]
-    best_model = GridSearchCV(LogisticRegression(), param_grid)
-    best_model.fit(X_train_val, y_train_val) 
-
-    print("\n\nBest model with best parameters on test set: ",
-          accuracy_score(y_test, best_model.predict(X_test)))
-    print("Best parameters of best model: ",best_model.best_params_)
-
-    return best_model
-
-def gradient_boosting():
-    param_grid = [
-        {"max_depth":[3,5,8]},
-        {"max_features":["log2","sqrt"]},
-        {"subsample":[0.5, 0.618, 0.8, 0.85, 0.9, 0.95, 1.0]}
-    ]
-    best_model = GridSearchCV(GradientBoostingClassifier(), param_grid)
-    best_model.fit(X_train_val, y_train_val) 
-
-    print("\n\nBest model with best parameters on test set: ",
-          accuracy_score(y_test, best_model.predict(X_test)))
-    print("Best parameters of best model: ",best_model.best_params_)
-
-    return best_model
+    test_accuracy = accuracy_score(y_test, current_model.predict(X_test))
+    print(f"\n{model_name} - Best parameters: {current_model.best_params_}, Test accuracy: {test_accuracy}")
+    
 
 #Classify samples on test_to_submit.csv
 #test_to_submit = pd.read_csv("dataset/test/test_to_submit.csv")
