0ce0760d277c143bb6bdde79615115799668c29a
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-12-13 10:11:19 +0100

    added best model

--- scripts/subSystem1/best_model.py
+++ scripts/subSystem1/best_model.py
@@ -1,9 +1,6 @@
 import joblib
 
-#Load information about the best model (from model1.py)
-best_model_data = joblib.load("scripts/subSystem1/best_model.pkl")
-print(f"Model Name: {best_model_data['model_name']}")
-print(f"Model: {best_model_data['model']}")
-print(f"Best Parameters: {best_model_data['best_params']}")
-print(f"Best Accuracy: {best_model_data['best_accuracy']}")
+def best_model():
+    best_model_data = joblib.load("scripts/subSystem1/best_model.pkl")
 
+    return best_model_data['model']
