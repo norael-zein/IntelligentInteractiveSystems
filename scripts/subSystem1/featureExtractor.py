ffd45f7969a6fbfd2026cfc97b21733699b5bf22
Paul Fessele <paul.fessele@icloud.com>
2024-12-13 10:59:29 +0100

    made attributes private

--- scripts/subSystem1/featureExtractor.py
+++ scripts/subSystem1/featureExtractor.py
@@ -13,10 +13,10 @@ class FeatureExtractor:
         Raises an exception if the webcam can't be opened
         """
 
-        self.detector = Detector(device='cuda' if torch.cuda.is_available() else 'cpu')
-        self.cap = cv2.VideoCapture(0)
+        self.__detector = Detector(device='cuda' if torch.cuda.is_available() else 'cpu')
+        self.__cap = cv2.VideoCapture(0)
 
-        if not self.cap.isOpened():
+        if not self.__cap.isOpened():
             raise Exception("Error (FeatureExtraction): Could not open webcam")
 
     def extract_action_units(self):
@@ -25,7 +25,7 @@ class FeatureExtractor:
         Returns None if no face is detected or an error occurs
         """
 
-        ret, frame = self.cap.read()
+        ret, frame = self.__cap.read()
 
         if not ret:
             print("Error (FeatureExtraction): Failed to capture frame")
@@ -37,7 +37,7 @@ class FeatureExtractor:
         pil_image.save(TEMP_IMAGE_PATH)
 
         try:
-            features = self.detector.detect_image([TEMP_IMAGE_PATH])
+            features = self.__detector.detect_image([TEMP_IMAGE_PATH])
             if not features.aus.empty:
                 return features.aus
             else:
@@ -53,7 +53,7 @@ class FeatureExtractor:
         to ensure that the webcam is released and temp files are removed
         """
 
-        self.cap.release()
+        self.__cap.release()
 
         if os.path.exists(TEMP_IMAGE_PATH):
             os.remove(TEMP_IMAGE_PATH)
