6c3168e104fd39c3d848c8901caa54c718f195f4
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-12-17 11:11:54 +0100

    added gesture.py

---
+++ scripts/subSystem2/gesture.py
@@ -0,0 +1,30 @@
+from furhat_remote_api import FurhatRemoteAPI
+from furhat_remote_api import Gesture
+
+"""
+Gestures made by Furhat during the interaction with the user 
+"""
+
+def breath():
+    pass
+
+def reflect():
+    pass
+
+def close_eyes():
+    pass
+
+def surprise():
+    pass
+
+def sad():
+    pass
+
+def angry():
+    pass
+
+def happy():
+    pass
+
+def listen():
+    pass
