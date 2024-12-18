7bc48041a3a38a02a2fede22bb0c5611c2c136d8
Nora El-Zein <34493664+norael-zein@users.noreply.github.com>
2024-12-18 22:35:25 +0100

    addedgestures

--- scripts/subSystem2/gesture.py
+++ scripts/subSystem2/gesture.py
@@ -1,5 +1,6 @@
 from furhat_remote_api import FurhatRemoteAPI
 from furhat_remote_api import Gesture
+import time
 
 # Create an instance of the FurhatRemoteAPI class, providing the address of the robot or the SDK running the virtual robot
 furhat = FurhatRemoteAPI("localhost")
@@ -9,7 +10,39 @@ furhat = FurhatRemoteAPI("localhost")
 Gestures made by Furhat during the interaction with the user 
 """
 def deep_breath():
-    pass
+        furhat.gesture(body={
+        "name": "ClosedEyes",
+        "frames": [
+            {
+                "time": [0, 1],
+                "persist": True,
+                "params": {
+                    "BLINK_LEFT": 1.0,  
+                    "BLINK_RIGHT": 1.0,  
+                    "SMILE_CLOSED": 0.5,  
+                    
+                }
+            },
+            {
+                  "time": [3,4],
+                  "persist": True,
+                  "params": {
+                        "PHONE_OOH_Q": 1
+                  }
+
+
+            },
+            {
+                "time": [6,7],
+                "persist": False,
+                "params": {
+                    "reset": True  
+                }
+            }
+        ],
+        "class": "furhatos.gestures.Gesture"
+    })
+print(deep_breath())
 
 def reflect():
     pass
@@ -19,24 +52,16 @@ def close_eyes():
         "name": "ClosedEyes",
         "frames": [
             {
-                "time": [0.5, 20],
+                "time": [0, 1],
                 "persist": True,
                 "params": {
                     "BLINK_LEFT": 1.0,  
                     "BLINK_RIGHT": 1.0,  
-                    "SMILE_CLOSED": 0.2,  
+                    "SMILE_CLOSED": 0.5,  
                 }
             },
             {
-                "time": [0, 20],
-                "persist": False,
-                "params": {
-                    "SMILE_CLOSED": 1
-
-                }
-            },
-            {
-                "time": [19, 20],
+                "time": [6,7],
                 "persist": False,
                 "params": {
                     "reset": True  
@@ -45,107 +70,8 @@ def close_eyes():
         ],
         "class": "furhatos.gestures.Gesture"
     })
-
-def surprise():
-    furhat.gesture(body={
-        "name":"Surprise",
-        "frames":[
-            {
-            "time":[0,2],
-            "persist":False,
-            "params":{
-                "SURPRISE": 0.5,
-                "PHONE_OH": 0.1,
-                "BROW_UP_LEFT": 0.8,
-                "BROW_UP_RIGHT": 0.8
-                }
-            },
-            {
-            "time":[0,3],
-            "persist":False,
-            "params":{
-                "reset":True
-                }
-            }],
-        "class":"furhatos.gestures.Gesture"
-        })
-
-def sad():
-    furhat.gesture(body={
-        "name":"ExpressSad",
-        "frames":[
-            {
-            "time":[0,2],
-            "persist":False,
-            "params":{
-                "PHONE_OOH_Q": 0.2,
-                "EYES_CLOSED": 1
-                }
-            },
-            {
-            "time":[0,4],
-            "persist":False,
-            "params":{
-                "reset":True
-                }
-            }],
-        "class":"furhatos.gestures.Gesture"
-        })
-
-def angry():
-    furhat.gesture(name="BrowFrown")
-    furhat.gesture(body={
-        "name":"ExpressAnger",
-        "frames":[
-            {
-            "time":[0,0.5],
-            "persist":False,
-            "params":{
-                "EXPR_ANGER":0.2,
-                "EXPR_DISGUST":0.1,
-                "BROW_IN_RIGHT":0.3,
-                "BROW_IN_LEFT": 0.3,
-                "NECK_PAN": -5
-                
-                }
-            },
-            {
-            "time":[0,2],
-            "persist":False,
-            "params":{
-                "reset":True
-                }
-            }],
-        "class":"furhatos.gestures.Gesture"
-        })
-
-def judge():
-    furhat.gesture(body={
-        "name":"BrowFrown",
-        "frames":[
-            {
-            "time":[0,1],
-            "persist":False,
-            "params":{
-                "EXPR_ANGER":0.2,
-                "EXPR_DISGUST":0.1,
-                "BROW_IN_RIGHT":0.5,
-                "BROW_IN_LEFT": 0.5,
-                "NECK_PAN": -2
-                
-                }
-            },
-            {
-            "time":[0,3],
-            "persist":False,
-            "params":{
-                "reset":True
-                }
-            }],
-        "class":"furhatos.gestures.Gesture"
-        })
-    
-def happy():
+        
+def smiling():
     furhat.gesture(body={
         "name":"BigSmile",
         "frames":[
@@ -153,10 +79,9 @@ def happy():
             "time":[0,1],
             "persist":False,
             "params":{
-                "BROW_UP_LEFT":1,
-                "BROW_UP_RIGHT":1,
-                "SMILE_OPEN":0.5,
-                "SMILE_CLOSED":0.7,
+                "BROW_UP_LEFT":0.8,
+                "BROW_UP_RIGHT":0.8,
+                "SMILE_CLOSED": 1,
                 "EYE_SQUINT_LEFT": 0.5,
                 "EYE_SQUINT_RIGHT": 0.5
                 }
@@ -216,39 +141,27 @@ def listen_nod_response():
             }],
         "class":"furhatos.gestures.Gesture"
         })
-    
-def test():
+
+def surprise():
     furhat.gesture(body={
-        "name": "CalmBreathing",
-        "frames": [
+        "name":"Surprise",
+        "frames":[
             {
-                "time": [0.5, 20],
-                "persist": True,
-                "params": {
-                    "BLINK_LEFT": 1.0,  
-                    "BLINK_RIGHT": 1.0,  
-                    "SMILE_CLOSED": 0.2,  
+            "time":[0,2],
+            "persist":False,
+            "params":{
+                "SURPRISE": 0.5,
+                "PHONE_OH": 0.1,
+                "BROW_UP_LEFT": 0.8,
+                "BROW_UP_RIGHT": 0.8
                 }
             },
             {
-                "time": [0, 20],
-                "persist": False,
-                "params": {
-                    "SMILE_CLOSED": 1
-
+            "time":[0,3],
+            "persist":False,
+            "params":{
+                "reset":True
                 }
-            },
-            {
-                "time": [19, 20],
-                "persist": False,
-                "params": {
-                    "reset": True  
-                }
-            }
-        ],
-        "class": "furhatos.gestures.Gesture"
-    })
-
-
-    
-print(test())
+            }],
+        "class":"furhatos.gestures.Gesture"
+        })   
