3381c4a16932560a035b35becd6b4d78d201db66
Paul Fessele <paul.fessele@icloud.com>
2025-01-09 14:28:34 +0100

    commented datasetProcessing.py

--- scripts/subSystem1/datasetProcessing.py
+++ scripts/subSystem1/datasetProcessing.py
@@ -1,4 +1,3 @@
-# Imports
 import os
 import pandas as pd
 from feat import Detector
@@ -20,40 +19,62 @@ data_sheet = pd.read_csv(input_data_sheet)
 detector = Detector(device='cpu')
 
 def main():
+    """
+    Processes the DiffusionFER dataset to be used for model training.
+    Extracts action units (AUs) from the images using the py-feat Detector class.
+    Two passes to process both the original and cropped images simultaneously.
+    """
 
+    # Store data as single-row dataframes
     aus_original = []
     aus_cropped = []
 
     # Iterate over the data sheet
     for index, row in data_sheet.iterrows():
 
+        # Construct the file paths
         image_path_full = row['subDirectory_filePath']
         image_name = image_path_full.split('/')[-1]
         folder_name = image_path_full.split('/')[-2]
         image_path_original = os.path.join(input_images_original, folder_name, image_name)
         image_path_cropped = os.path.join(input_images_cropped, folder_name, image_name)
 
-        # Detect faces and emotions
+        # Detect faces and emotions for images in original folder
         try:
             predictions_original = detector.detect_image([image_path_original])
+
+            # Check if a face was detected
             if not predictions_original.aus.empty:
+                # Create new dataframe by appending the action units to the information from the corresponding row
                 new_row_original = pd.concat([row.to_frame().T.reset_index(drop=True), predictions_original.aus],
                                              axis=1)
+                # Replace numerical value with string label
                 new_row_original['expression'] = folder_name
+                # Append dataframe
                 aus_original.append(new_row_original)
         except Exception as e:
-            print(e)
+            print("Error (DatasetProcessing): {}".format(e))
 
+        # Detect faces and emotions for images in cropped folder
         try:
             predictions_cropped = detector.detect_image([image_path_cropped])
+
+            # Check if a face was detected
             if not predictions_cropped.aus.empty:
+                # Create new dataframe by appending the action units to the information from the corresponding row
                 new_row_cropped = pd.concat([row.to_frame().T.reset_index(drop=True), predictions_cropped.aus], axis=1)
+                # Replace numerical value with string label
                 new_row_cropped['expression'] = folder_name
+                # Append dataframe
                 aus_cropped.append(new_row_cropped)
         except Exception as e:
-            print(e)
+            print("Error (DatasetProcessing): {}".format(e))
 
-    # Combine all rows into single DataFrames and save them
+    """
+    Combine all rows into single DataFrames and save them.
+    Use .dropna() to filter out rows with invalid or null entries.
+    Happens as result of inconsistent dataset, original folder has more images than cropped folder
+    """
     if aus_original:
         df_original = pd.concat(aus_original, ignore_index=True)
         df_original.dropna().to_csv(output_aus_original, index=False)
